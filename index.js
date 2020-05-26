3cc81f5381ef70bfbe76e9a29fa5ecf79825e4d6
Gabriel Machado <gadu.machado@gmail.com>
2020-05-26 17:26:27 -0300

    fixed visits bug

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const fs = require('fs')
 
+
+
 const app = express();
 let visits;
 fs.readFile('visits.json', (err, data) => {
@@ -9,10 +11,21 @@ fs.readFile('visits.json', (err, data) => {
         visits = { data: [] };
     } else {
         visits = JSON.parse(data);
-        console.log(visits);
     }
 });
 
+//counting visits
+app.get('/', function (res, req, next) {
+    let visit = {
+        date: new Date(),
+        ip: req.connection.remoteAddress
+    }
+    visits.data.push(visit)
+    let data = JSON.stringify(visits);
+    fs.writeFileSync('visits.json', data);
+    console.log('hello')
+    next()
+})
 
 //static server
 app.use('/', express.static(__dirname + '/build'));
@@ -27,20 +40,10 @@ app.get('/visits/data', (req, res, next) => {
     res.json(visits)
 })
 
-//counting visits
-app.use('/', function (res, req, next) {
-    let visit = {
-        date: new Date(),
-        ip: req.connection.remoteAddress
-    }
-    visits.data.push(visit)
-    let data = JSON.stringify(visits);
-    fs.writeFileSync('visits.json', data);
-    next()
-})
 
 //default route
 app.use('/', function (req, res, next) {
+    console.log('hello')
     res.sendFile(__dirname + '/build/index.html');
 })
 
